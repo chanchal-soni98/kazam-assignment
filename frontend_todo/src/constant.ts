3693a5ff0eb36b97bb497af4b79c0568d788900a
chanchalsoni1998@gmail.com <chanchalsoni1998@gmail.com>
2025-05-10 18:14:08 +0530

    UI changes

--- frontend_todo/src/constant.ts
+++ frontend_todo/src/constant.ts
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL = "http://localhost:3000/api";
+export const API_BASE_URL = "https://kazam-assignment-0dbi.onrender.com/api";
 
 export const MQTT_URL = "wss://broker.hivemq.com:8884/mqtt";
 export const MQTT_SUB_TOPIC = "/add";
